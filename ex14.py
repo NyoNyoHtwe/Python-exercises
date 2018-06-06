f3b1461e6401ae161382eda3686a2de5a099bcc7
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 11:27:52 -0800

    commit ex14

---
+++ ex14.py
@@ -0,0 +1,7 @@
+from sys import argy
+script, first, second, third=argy
+print ("The script is called:",script)
+print ("Your first variable is:",first)
+print ("Your second variable is:",second)
+print ("Your third variable is:",third)
+
