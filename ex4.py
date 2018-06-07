bf47fd6347f1ce4ecc5461bd4f4ca8f64d7738f6
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 21:31:39 -0800

    add ex4

--- ex4.py
+++ ex4.py
@@ -4,7 +4,7 @@ drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
-carpool _ capacity = cars _ driven * space _ in _ a _ car
+carpool_capacity = cars_driven * space _ in _ a _ car
 average_passengers_per_car =passengers / cars_driven
 print ("There are",cars,"cars available.")
 print ("There are only",drivers,"drivers available.")
