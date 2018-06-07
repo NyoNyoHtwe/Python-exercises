300c5aa5bf3ce532da287e7f5bff0e8111510839
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 22:30:11 -0800

    add ex4

--- ex4.py
+++ ex4.py
@@ -1,5 +1,5 @@
 cars = 100
-space_in_car = 4
+space_in_a_car = 4
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
@@ -11,5 +11,5 @@ print ("There are only",drivers,"drivers available.")
 print ("There will be",cars_not_driven,"empty cars today.")
 print ("we can transport", carpool_capacity,"people today.")
 print ("We have",passengers,"to carpool today.")
-print ("we need to put about", average_passengers-per_car,"in each car.")
+print ("we need to put about", average_passengers_per_car,"in each car.")
 
