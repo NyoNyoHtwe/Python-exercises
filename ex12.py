44f5e327ff14c51e1d2da31fbba8c8e97819bbc4
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 22:52:38 -0800

    add ex12

--- ex12.py
+++ ex12.py
@@ -1,8 +1,8 @@
 print ("How old are you?"),
-age= raw_input()
+age= input()
 print ("How tall are you?"),
-height = raw=input()
+height = input()
 print ("How much do you weight?"),
-weight = raw_input()
+weight = input()
 print ("So, you're %r old, %r tall and %r heavy." % (age,height,weight)")
 
