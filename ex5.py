52c40d36d99ecea8e17593fd255c7b44eb35b63f
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 21:18:15 -0800

    add ex5

--- ex5.py
+++ ex5.py
@@ -11,5 +11,5 @@ print ("He's %d pounds heavy." % my_weight)
 print ("Actually that's not too heavy.")
 print ("He's got %s eyes and %s hair." %(my_eyes,my_hair))
 print ("His teeth are usually %s depending on the coffee." % my_teeth)
-print ("If I add %d and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
+print ("If I add %d and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight))
 
