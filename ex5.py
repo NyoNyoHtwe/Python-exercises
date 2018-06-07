6b66e9105d35acc1719c538faea518eee5669119
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-06 21:21:31 -0800

    add ex5

--- ex5.py
+++ ex5.py
@@ -3,7 +3,7 @@ my_age=35
 my_height=74
 my_weight=180
 my-eyes='Blue'
-my_teeth='white'
+my_teeth='White'
 my_hair='Brown'
 print ("Let's talk about %s."% my_name)
 print ("He's %d inches tall." % my_height)
@@ -11,5 +11,5 @@ print ("He's %d pounds heavy." % my_weight)
 print ("Actually that's not too heavy.")
 print ("He's got %s eyes and %s hair." %(my_eyes,my_hair))
 print ("His teeth are usually %s depending on the coffee." % my_teeth)
-print ("If I add %d and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight))
+print ("If I add %d and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
 
