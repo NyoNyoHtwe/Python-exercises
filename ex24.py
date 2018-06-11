50e393f60f8be43b64257eb1088c41c79e7489ad
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-10 21:36:52 -0800

    commit ex20

--- ex24.py
+++ ex24.py
@@ -1,5 +1,5 @@
 print ("Let's practice everything.")
-print ('You\ 'd need to know \'but escapes with \\ that do:')
+print ('You\'d need to know \'but escapes with \\ that do:')
 print ('\n newlines and \t tabs.')
 
 poem = """
@@ -20,10 +20,10 @@ five = 10 - 2 + 3 - 6
 print (f"This should be five: {five}")
 
 def secret_formula(started):
-    jelly_bean = started * 500
+    jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 100
-    return jelly_beans, jars, creats
+    return jelly_beans, jars, crates
 
 
 start_point = 10000
@@ -33,14 +33,14 @@ beans, jars, crates = secret_formula(start_point)
 print ("With a starting point of: {}".format(start_point))
 
 
-print (f"We'd have {beans} beans, {jars} jars, and {creates} creates.")
+print (f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print ("We can also do that this way:")
 formula = secret_formula(start_point)
 
-print ("We'd have {} beans, {} jars, and {} creates.".format(*formula))
+print ("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
         
 
