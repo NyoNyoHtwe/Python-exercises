24106aa4d0ff377e436f18d3ae5b376a6ecece41
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-12 14:50:31 -0800

    commit ex25

--- ex25.py
+++ ex25.py
@@ -32,6 +32,6 @@ def print_first_and_last(sentence):
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one ."""
     words = sort_sentence(sentence)
-    print_first_:word(words)
+    print_first_word(words)
     print_last_word(words)
 
