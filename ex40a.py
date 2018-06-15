abc88cc501d45ee69e3941b3024a008c16e69c20
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-14 16:18:40 -0800

    commit ex40a

---
+++ ex40a.py
@@ -0,0 +1,46 @@
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+
+def apple():
+    print ("I AM APPLES!")
+
+import mystuff
+mystuff.apple()
+
+def apple():
+    print("I AM APPLES!")
+
+tangerine = "Living reflection of a dream"
+
+import mystuff
+
+mystuff.apple()
+print(mystuff.tangerine)
+
+mystuff['apple']
+mystuff.apple()
+mystuff.tangerine
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years betwwen"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+mystuff['apple']
+
+mystuff.apples()
+print(mystuff.tangerine)
+
+thing = MyStuff()
+thing.apples()
+print(thing.tangerine)
+
