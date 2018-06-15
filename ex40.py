ba86379c9c61518edd8a7b167dd90dd1caea1750
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-14 16:35:15 -0800

    commit ex40

--- ex40.py
+++ ex40.py
@@ -4,10 +4,10 @@ class Song(object):
         self.lyrics = lyrics
 
     def sing_me_a_song(self):
-        for line in self.lurics:
-            print(Line)
+        for line in self.lyrics:
+            print(line)
 
-happy_baby = Song(["Happy birthday to you",
+happy_bday = Song(["Happy birthday to you",
                     "I don't want to get sued",
                     "So I'll stop right there"])
 
