946fd39c882e4bcbe9e01a3a0db6ca8272e7d718
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-16 09:53:48 -0800

    commit words

--- ex41.py
+++ ex41.py
@@ -1,11 +1,11 @@
-import random 
-from urllib.request import urlopen 
-import sys
+ import random 
+ from urllib.request import urlopen 
+ import sys
 
-WORD_URL = "http://learncodethehardway.org/words.txt" 
-WORDS = []
+ WORD_URL = "http://learncodethehardway.org/words.txt" 
+ WORDS = []
 
-PHRASES = { 
+ PHRASES = { 
         "class %%%(%%%):": 
         "Make a class named %%% that is-a %%%.", 
         "class %%%(object):\n\tdef __init__(self, ***)" : 
