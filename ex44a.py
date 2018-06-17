b259a2228c0000b78fedd5dbe6b80deddf7643cf
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-16 21:00:50 -0800

    commit ex44a

---
+++ ex44a.py
@@ -0,0 +1,16 @@
+class Parent(object):
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+class Chils(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dat.implicit()
+som.implicit()
+
+ 
+
