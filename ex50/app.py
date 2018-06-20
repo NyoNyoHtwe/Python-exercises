ef060239dcf2d21ee5cde8a1326af6d130d662ed
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-19 19:13:41 -0800

    add app.py

--- ex50/app.py
+++ ex50/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import render_templates
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     greenting = "Hello World"
-    return render_templates("index.html", greenting=greenting)
+    return render_template("index.html", greenting=greenting)
 
 if __name__ == "__main__":
     app.run()
